2fc70a7b6947033cf00dc596a8f6d51e2df33b51
abodiamhe <akhigbesadiq@gmail.com>
2024-07-03 22:02:30 +0100

    third commit

--- index.js
+++ index.js
@@ -12,24 +12,23 @@ const appKey = process.env.API_KEY;
 app.get("/api/hello", async (req, res) => {
   const visitor = req.query.visitor_name;
 
-  const clientIp = await axios.get("http://ip-api.com/json/");
+  const clientIp = await axios.get("https://ipapi.co/json/"); //http://ip-api.com/json/
   const clientIpData = await clientIp.data;
   console.log(clientIpData);
+
   const city = clientIpData.city;
-  const query = clientIpData.query;
+  const ip = clientIpData.ip;
   // `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appKey}&units=metric`;
 
-  if (clientIpData.status === "success") {
-    const response = await axios.get(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${appKey}&units=metric`
-    );
-    const resData = await response.data;
-    res.json({
-      client_ip: query,
-      location: city,
-      greeting: `Hello, ${visitor}!, the temperature is ${resData.main.temp} degrees Celcius in ${city}`,
-    });
-  }
+  const response = await axios.get(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${appKey}&units=metric`
+  );
+  const resData = await response.data;
+  res.json({
+    client_ip: ip,
+    location: city,
+    greeting: `Hello, ${visitor}!, the temperature is ${resData.main.temp} degrees Celcius in ${city}`,
+  });
 });
 
 app.listen(port, () => {
