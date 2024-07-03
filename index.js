ec094db68ef49ed903e8d8e0830fff0b6cc5db19
abodiamhe <akhigbesadiq@gmail.com>
2024-07-03 20:39:46 +0100

    third commit

--- index.js
+++ index.js
@@ -9,40 +9,27 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const appKey = process.env.API_KEY;
 
-const fetchClientId = async () => {
-  const clientIp = await axios.get("http://ip-api.com/json");
-  const clientIpData = await clientIp.data;
-  const lat = clientIpData.lat;
-  const lon = clientIpData.lon;
-  const city = clientIpData.city;
-  const query = clientIpData.query;
-
-  return {
-    lat,
-    lon,
-    city,
-    query,
-  };
-};
-
 app.get("/api/hello", async (req, res) => {
   const visitor = req.query.visitor_name;
-  const { lat, lon, city, query } = await fetchClientId();
 
-  if ((lat, lon, city, query)) {
-    try {
-      const response = await axios.get(
-        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appKey}&units=metric`
-      );
-      const resData = await response.data;
-      res.json({
-        client_ip: query,
-        location: city,
-        greeting: `Hello, ${visitor}!, the temperature is ${resData.main.temp} degrees Celcius in ${city}`,
-      });
-    } catch (error) {
-      res.status(404).send(error.message);
-    }
+  const clientIp = await axios.get("http://ip-api.com/json/");
+  const lat = clientIp.data.lat;
+  const lon = clientIp.data.lon;
+  const city = clientIp.data.city;
+  const query = clientIp.data.query;
+
+  try {
+    const response = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appKey}&units=metric`
+    );
+    const resData = await response.data;
+    res.json({
+      client_ip: query,
+      location: city,
+      greeting: `Hello, ${visitor}!, the temperature is ${resData.main.temp} degrees Celcius in ${city}`,
+    });
+  } catch (error) {
+    console.log(error);
   }
 });
 
