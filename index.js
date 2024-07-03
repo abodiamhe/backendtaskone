bd78bf018b7762cd7f4f188ab1b36f098842267d
abodiamhe <akhigbesadiq@gmail.com>
2024-07-03 10:16:50 +0100

    add client Id

--- index.js
+++ index.js
@@ -9,24 +9,25 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const query = "New York";
 const appKey = process.env.API_KEY;
-const unit = "metric";
-const url =
-  "https://api.openweathermap.org/data/2.5/weather?q=" +
-  query +
-  "&appid=" +
-  appKey +
-  "2&units=" +
-  unit;
 
 app.get("/api/hello", async (req, res) => {
   const visitor = req.query.visitor_name;
-  try {
-    const response = await axios.get(url);
+
+  const clientIp = await axios.get("http://ip-api.com/json");
+  const lat = clientIp.data.lat;
+  const lon = clientIp.data.lon;
+  const city = clientIp.data.city;
+  const query = clientIp.data.query;
+
+  http: try {
+    const response = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appKey}&units=metric`
+    );
     const resData = await response.data;
     res.json({
-      client_ip: "127.0.0.1",
-      location: query,
-      greeting: `Hello, ${visitor}!, the temperature is ${resData.main.temp} degrees Celcius in ${query}`,
+      client_ip: query,
+      location: city,
+      greeting: `Hello, ${visitor}!, the temperature is ${resData.main.temp} degrees Celcius in ${city}`,
     });
   } catch (error) {
     res.status(404).send(error.message);
