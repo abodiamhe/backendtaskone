ce29a09e25373cfc03e65496ff7e0480c514c9dc
abodiamhe <akhigbesadiq@gmail.com>
2024-07-04 17:43:00 +0100

    remove /

--- index.js
+++ index.js
@@ -10,25 +10,30 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const appKey = process.env.API_KEY;
 const token = process.env.TOKEN;
 
-app.get("/api/hello", async (req, res) => {
-  const visitor = req.query.visitor_name;
-
+const generateIp = async () => {
   const clientIp = await axios.get(`https://ipinfo.io/json?token=${token}`); //http://ip-api.com/json/
   const clientIpData = await clientIp.data;
 
-  const city = clientIpData.city;
-  const ip = clientIpData.ip;
-  try {
-    const response = await axios.get(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${appKey}&units=metric`
-    );
-    const resData = await response.data;
-    res.json({
-      client_ip: ip,
-      location: city,
-      greeting: `Hello, ${visitor}!, the temperature is ${resData.main.temp} degrees Celcius in ${city}`,
-    });
-  } catch (error) {}
+  return {
+    city: clientIpData.city,
+    ip: clientIpData.ip,
+  };
+};
+
+app.get("/api/hello", async (req, res) => {
+  const visitor = req.query.visitor_name;
+  const { city, ip } = await generateIp();
+  console.log(city, ip);
+
+  const response = await axios.get(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${appKey}&units=metric`
+  );
+  const resData = await response.data;
+  res.json({
+    client_ip: ip,
+    location: city,
+    greeting: `Hello, ${visitor}!, the temperature is ${resData.main.temp} degrees Celcius in ${city}`,
+  });
 });
 
 app.listen(port, () => {
